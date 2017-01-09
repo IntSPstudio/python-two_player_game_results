efc44ae1c8bdf999fec92987bcb28154c045135b
IntSPstudio <official.intsp@gmail.com>
2017-01-09 21:49:02 +0200

    0.0.5.20170109 - 21.9800.217-0109.5.10
    Using It8c library's new feature

--- 2pgr/twoplayergameresults.py
+++ 2pgr/twoplayergameresults.py
@@ -2,7 +2,7 @@
 # Made by IntSPstudio
 # Two Player Game Results
 # Thank you for using this software!
-# Version: 0.0.4.20170108
+# Version: 0.0.5.20170109
 # ID: 980005010
 #
 # Twitter: @IntSPstudio
@@ -17,8 +17,8 @@ def getContentCheck(array1Content):
 	#LIMITS
 	array1Height = len(array1Content)
 	array1Width =  len(array1Content[0])
-	array2Height = 1
-	array2Width = 3
+	array2Height =1
+	array2Width =3
 	#CONTENT
 	ypb =1
 	if array1Width > 2:
@@ -96,6 +96,7 @@ if __name__ == "__main__":
 		#INPUT
 		rawArrayContent = it8c.csvReadFile(fileNameI,";")
 		mainArrayContent = getContentCheck(rawArrayContent)
+
 		#CLASSES
 		classRawContent = extractContentLine(mainArrayContent,2)
 		classRefContent = it8c.dataExtractArrayColumn(it8c.dataCheckListObjects(it8c.dataChangeListContentFormat(it8c.dataChangeListContentFormat(classRawContent,1),3)),2)
@@ -103,33 +104,42 @@ if __name__ == "__main__":
 		classCounter = len(classRefContent)
 		classWinRate = getRoundsWinRate(mainArrayContent)
 		#PLAYER 1
-		player1Name = str.upper(getPlayerName(mainArrayContent,0))
+		player1Name = getPlayerName(mainArrayContent,0)
 		player1Content = extractContentLine(mainArrayContent,0)
 		player1Total = it8c.dataCalcSumList(player1Content)
-		player1Avarage = it8c.dataCalcAvgList(player1Content)
+		player1Avarage = int(it8c.dataCalcAvgList(player1Content))
 		player1Rounds = len(player1Content)
 		player1RoundsWinR = classWinRate[0]
 		#PLAYER 2
-		player2Name = str.upper(getPlayerName(mainArrayContent,1))
+		player2Name = getPlayerName(mainArrayContent,1)
 		player2Content = extractContentLine(mainArrayContent,1)
 		player2Total = it8c.dataCalcSumList(player2Content)
-		player2Avarage = it8c.dataCalcAvgList(player2Content)
+		player2Avarage = int(it8c.dataCalcAvgList(player2Content))
 		player2Rounds = len(player2Content)
 		player2RoundsWinR = classWinRate[1]
-		#PRINT
+		#CONTENT ARRAY
+		printContent = it8c.dataCreateArray(7,3,"")
+		#TITLE
+		printContent[0][0] = "Name:"
+		printContent[1][0] = "Total:"
+		printContent[2][0] = "Avarage:"
+		printContent[3][0] = "Rounds won:"
+		#PLAYER CONTENT
+		printContent[0][1] = player1Name
+		printContent[1][1] = player1Total
+		printContent[2][1] = player1Avarage
+		printContent[3][1] = player1RoundsWinR
+		printContent[0][2] = player2Name
+		printContent[1][2] = player2Total
+		printContent[2][2] = player2Avarage
+		printContent[3][2] = player2RoundsWinR
+		#OTHER
+		printContent[5][0] = "Rounds:"
+		printContent[5][1] = classRounds
+		if classRounds != classCounter:
+			printContent[6][0] = "Classes:"
+			printContent[6][1] = classCounter 
 		print(it8c.vslTerminalLine(0,""))
-		print(player1Name)
-		print("Total:", player1Total)
-		print("Avarage:", player1Avarage)
-		print("Rounds won:", player1RoundsWinR)
-		print()
-		print(player2Name)
-		print("Total:", player2Total)
-		print("Avarage:", player2Avarage)
-		print("Rounds won:", player2RoundsWinR)
-		print()
-		if classRounds == classCounter:
-			print("Rounds:", classRounds)
-		else:
-			print("Rounds:", classRounds)
-			print("Classes:", classCounter)
+		print(it8c.dataSmrPrintArray(printContent,"","",0))
+		#SAVE
+		#it8c.csvWriteFile(printContent,"results.csv",";","")
